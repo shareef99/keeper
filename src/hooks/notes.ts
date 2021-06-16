4be516bfc8605c82e5ed8f6629bdfa38a091bceb
shareef <nadeemshareef934@gmail.com>
2021-06-16 21:18:18 +0530

    add custom hook to fetch the notes
    Hook name is useFetchNotes

---
+++ src/hooks/notes.ts
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import { useAuth } from "../context/AuthContext";
+import { getNotesRef } from "../helpers/notes";
+import { NoteType } from "../interface";
+
+export const useFetchNotes = () => {
+    const [notes, setNotes] = useState<Array<NoteType>>([]);
+    const { user } = useAuth();
+
+    useEffect(() => {
+        getNotesRef(user?.uid!).onSnapshot((snap) =>
+            setNotes(
+                snap.docs.map((doc) => ({
+                    id: doc.data().id,
+                    title: doc.data().title,
+                    content: doc.data().content,
+                    createdAt: doc.data().createdAt,
+                    lastEditedAt: doc.data().lastEditedAt || "Original",
+                }))
+            )
+        );
+    }, [user?.uid]);
+
+    return notes;
+};
--- src/keeper/Notes.tsx
+++ src/keeper/Notes.tsx
@@ -1,37 +1,14 @@
 import { useAuth } from "../context/AuthContext";
 import CreateNote from "../components/Notes/CreateNote";
-import { NoteType } from "../interface";
-import { useEffect, useState } from "react";
 import Note from "../components/Notes/Note";
-import { db } from "../firebase/config";
 import Zoom from "@material-ui/core/Zoom";
+import { useFetchNotes } from "../hooks/notes";
 
 interface Props {}
 
 const Notes = (props: Props) => {
-    // Context
-    const { user, logout } = useAuth();
-
-    // State
-    const [notes, setNotes] = useState<Array<NoteType>>([]);
-
-    // Effects
-    useEffect(() => {
-        db.collection("users")
-            .doc(user?.uid)
-            .collection("notes")
-            .onSnapshot((snap) =>
-                setNotes(
-                    snap.docs.map((doc) => ({
-                        id: doc.data().id,
-                        title: doc.data().title,
-                        content: doc.data().content,
-                        createdAt: doc.data().createdAt,
-                        lastEditedAt: doc.data().lastEditedAt || "Original",
-                    }))
-                )
-            );
-    }, [user?.uid]);
+    const { logout } = useAuth();
+    const notes = useFetchNotes();
 
     return (
         <section>
