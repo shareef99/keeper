4d4119fe5b51a07ebb64bdf509583a934a237291
shareef <nadeemshareef934@gmail.com>
2021-06-18 16:27:55 +0530

    add labels

--- src/components/Notes/components/LabelMenu.tsx
+++ src/components/Notes/components/LabelMenu.tsx
@@ -2,12 +2,30 @@ import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import Fade from "@material-ui/core/Fade";
 import { MouseEvent, useState } from "react";
+import Input from "@material-ui/core/Input";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { BiSearch } from "react-icons/bi";
+import { useEffect } from "react";
+import { width } from "@material-ui/system";
 
 interface Props {}
 
 const LabelMenu = (props: Props) => {
     // State
     const [labelEl, setLabelEl] = useState<null | HTMLElement>(null);
+    const [labels, setLabels] = useState<Array<string>>([
+        "Blogs",
+        "Notes",
+        "College",
+        "School",
+    ]);
+    const [currentNoteLabel, setCurrentNoteLabel] = useState<Array<string>>([]);
+
+    // Effects
+    useEffect(() => {
+        console.log(currentNoteLabel);
+    }, [currentNoteLabel]);
 
     // Handlers
     const openLabelMenu = (e: MouseEvent<HTMLLIElement>) => {
@@ -44,9 +62,40 @@ const LabelMenu = (props: Props) => {
                     horizontal: "right",
                 }}
             >
-                <MenuItem>Testing 1 2 3</MenuItem>
-                <MenuItem>Testing 1 2 3</MenuItem>
-                <MenuItem>Testing 1 2 3</MenuItem>
+                <MenuItem disabled className="disableMenuItem">
+                    Label note
+                </MenuItem>
+                <MenuItem>
+                    <Input placeholder="Enter label name" />
+                    <BiSearch />
+                </MenuItem>
+                {labels.map((label) => (
+                    <MenuItem>
+                        <FormControlLabel
+                            style={{ width: `100%` }}
+                            control={
+                                <Checkbox
+                                    color="default"
+                                    value={label}
+                                    onChange={(e) => {
+                                        if (currentNoteLabel.includes(label)) {
+                                            return setCurrentNoteLabel(
+                                                currentNoteLabel.filter(
+                                                    (x) => x !== label
+                                                )
+                                            );
+                                        }
+                                        setCurrentNoteLabel((prevLabels) => [
+                                            ...prevLabels,
+                                            e.target.value,
+                                        ]);
+                                    }}
+                                />
+                            }
+                            label={label}
+                        />
+                    </MenuItem>
+                ))}
             </Menu>
         </>
     );
